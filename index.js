e462c944e5b93953d900e51b5622807c283fea64
lilongllong <2453085960@qq.com>
2017-01-10 14:54:33 +0800

    Add init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send(`Hello Docker`);
+});
+
+app.listen(8080, () => {
+    console.log("server is running.");
+});
