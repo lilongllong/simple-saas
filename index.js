6e20d1c28fef0685debf456c57a0a1820b7120d0
lilongllong <2453085960@qq.com>
2017-01-10 16:40:24 +0800

    Completed docker test.

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require("express");
 
 const app = express();
 
-const COMPANY_NAME = process.env.COMPANY_NAME;
+const COMPANY_NAME = process.env.COMPANY_NAME ? process.env.COMPANY_NAME : "cassiny";
+
+const MONGODB_ADDR = process.env.MONGODB_PORT_27017_TCP_ADDR ? process.env.MONGODB_PORT_27017_TCP_ADDR : "localhost";
+const MONGODB_PORT = process.env.MONGODB_PORT_27017_TCP_PORT ? process.env.MONGODB_PORT_27017_TCP_PORT : "localhost";
 
 app.get("/", (req, res) => {
-    res.send(`Hello ${ COMPANY_NAME ? COMPANY_NAME : "stranger"}!`);
+    res.type("text/html");
+    res.send(`
+        <h1>${COMPANY_NAME}</h1>
+        <ul>
+            <li>Host: ${MONGODB_ADDR}</li>
+            <li>Port: ${MONGODB_PORT}</li>
+        </ul>
+        `);
 });
 
 app.listen(8080, () => {
